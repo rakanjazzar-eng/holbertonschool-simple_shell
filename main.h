5ac235d862f6db6c0e917e4618667410c039e86a
Kony Jazzar <rakanjazzar@gmail.com>
2026-08-24 18:16:47 +0300

    fix main.h include guard

--- main.h
+++ main.h
@@ -1,47 +1,27 @@
-#include "main.h"
- 
-char *prog_name;
- 
-/**
- * main - entry point of the simple shell
- * @argc: argument count (unused)
- * @argv: argument vector, argv[0] is the name the shell was invoked with
- *
- * Return: always 0
- */
-int main(int argc, char **argv)
-{
-	char *line;
-	char *args[2];
-	int interactive = isatty(STDIN_FILENO);
- 
-	(void)argc;
-	prog_name = argv[0];
- 
-	while (1)
-	{
-		if (interactive)
-			write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
- 
-		line = getln();
-		if (line == NULL)
-		{
-			if (interactive)
-				write(STDOUT_FILENO, "\n", 1);
-			break;
-		}
- 
-		if (line[0] == '\0')
-		{
-			free(line);
-			continue;
-		}
- 
-		args[0] = line;
-		args[1] = NULL;
-		launch_process(args);
-		free(line);
-	}
- 
-	return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+
+extern char **environ;
+extern char *prog_name;
+
+#define PROMPT "($) "
+
+char *getln(void);
+char **formatln(char *ln);
+char *compose(char *token, char *val);
+void launch_process(char **command);
+void search_exe(char **command);
+char *_getenv(char *name);
+void handler_function(int i);
+
+#endif /* MAIN_H */
