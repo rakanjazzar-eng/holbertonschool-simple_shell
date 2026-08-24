f960bf5b7d4e25d753d816f74298b02c22302b16
Kony Jazzar <rakanjazzar@gmail.com>
2026-08-24 17:25:28 +0300

    main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+ #ifndef MAIN_H
+#define MAIN_H
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+ 
+/* Function prototypes will be added here as the shell grows */
+ 
+#endif /* MAIN_H */
