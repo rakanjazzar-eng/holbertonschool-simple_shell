31fba90f96c67a0465473991cdabd9139cd28c6a
Kony Jazzar <rakanjazzar@gmail.com>
2026-08-25 15:44:49 +0300

    fix getln.c

--- Getln .c
+++ Getln .c
@@ -1,16 +1,17 @@
-
 #include "main.h"
  
 /**
- * getln - reads a line of input from standard input
+ * getln - reads a line of input from standard input, trimming any
+ * leading and trailing whitespace (spaces, tabs, newline)
  *
- * Return: a pointer to the dynamically allocated line (without its
- * trailing newline), or NULL if end of file was reached
+ * Return: a pointer to the dynamically allocated, trimmed line, or
+ * NULL if end of file was reached
  */
 char *getln(void)
 {
 	char *line = NULL;
-	size_t len = 0;
+	char *start;
+	size_t len = 0, end;
 	ssize_t nread;
  
 	nread = getline(&line, &len, stdin);
@@ -20,9 +21,18 @@ char *getln(void)
 		return (NULL);
 	}
  
-	if (nread > 0 && line[nread - 1] == '\n')
-		line[nread - 1] = '\0';
+	start = line;
+	while (*start == ' ' || *start == '\t')
+		start++;
+ 
+	end = strlen(start);
+	while (end > 0 && (start[end - 1] == '\n' || start[end - 1] == ' ' ||
+			start[end - 1] == '\t' || start[end - 1] == '\r'))
+		end--;
+	start[end] = '\0';
+ 
+	if (start != line)
+		memmove(line, start, end + 1);
  
 	return (line);
 }
- 
